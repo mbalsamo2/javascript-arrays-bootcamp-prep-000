4794de088d59fca407ea38a774bf3e71fa2d4d79
Maria Balsamo <mia.balsamo@gmail.com>
2017-12-04 16:06:02 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,22 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, element) {
+  array = [1];
+  array = ['foo',...array];
+  return array;
+  
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+ array = [1];
+ array.unshift('foo');
+ return array;
+}
+
+function addElementToEndOfArray() {
+  
+}
+
+function destructivelyAddElementToEndOfArray() {
+  
+}
